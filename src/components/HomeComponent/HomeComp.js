9a6f8bcca9949385bfa7e619ac2c50d3368fa43a
Anthony Parrino <anthony21parrino21@gmail.com>
2021-09-16 18:07:08 -0400

    created react app and starter file structure

---
+++ src/components/HomeComponent/HomeComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homeComp.css';
+
+function HomeComp() {
+    return (
+        <div>
+            home page
+        </div>
+    )
+}
+
+export default HomeComp
