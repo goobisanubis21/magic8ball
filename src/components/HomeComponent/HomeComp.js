f550f4f496fefde2886b74f82e33c21620a719ef
Anthony Parrino <anthony21parrino21@gmail.com>
2021-09-16 21:05:56 -0400

    added html elements to web app

--- src/components/HomeComponent/HomeComp.js
+++ src/components/HomeComponent/HomeComp.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './homeComp.css';
+import Ball from './android-chrome-512x512.png'
 
 function HomeComp() {
     return (
-        <div>
-            home page
+        <div className='main-home-page-div'>
+            <div className='title-div'>
+                <h1 className='title'>Magic 8-Ball</h1>
+            </div>
+            <div className='image-div'>
+                <img className='image' src={Ball} alt='...'></img>
+            </div>
+            <div className='result-div'>
+                <p className='result-text'>results go here</p>
+            </div>
+            <div className='input-div'>
+                <form className='input-form'>
+                    <input className='input-box' placeholder='Ask Me Anything'></input>
+                    <div className='submit-btn-div'>
+                        <button className='submut-btn'>SUBMIT</button>
+                    </div>
+                    <div className='history-btn-div'>
+                        <button className='history-btn'>HISTORY</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
