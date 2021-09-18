d3f76556719759eab2dcd55a6231db2e9bad0c12
Anthony Parrino <anthony21parrino21@gmail.com>
2021-09-18 12:01:55 -0400

    connected api for responses when asking questions

--- src/components/HomeComponent/HomeComp.js
+++ src/components/HomeComponent/HomeComp.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './homeComp.css';
 import Ball from './android-chrome-512x512.png'
 
 function HomeComp() {
+
+    const [answer, setAnswer] = useState("All of your Questions will be Answered");
+    let inputQuestion = useRef();
+
+    function onSubmit(e) {
+        e.preventDefault()
+
+        if(inputQuestion.current.value === '') {
+            setAnswer('You Must Not Leave the Question Blank')
+            return
+        }
+
+        if (inputQuestion.current.value.split("")[inputQuestion.current.value.length-1] !== '?') {
+            setAnswer('I only Accept Questions')
+            return
+        }
+
+        let params = inputQuestion.current.value
+        let uri = "https://8ball.delegator.com/magic/JSON/" + params;
+        fetch(uri)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setAnswer(json.magic.answer)
+            }).catch(err => {
+                console.log(err)
+            });
+    }
+
     return (
         <div className='main-home-page-div'>
             <div className='title-div'>
@@ -12,13 +41,13 @@ function HomeComp() {
                 <img className='image' src={Ball} alt='...'></img>
             </div>
             <div className='result-div'>
-                <p className='result-text'>results go here</p>
+                <p className='result-text'>{answer}</p>
             </div>
             <div className='input-div'>
                 <form className='input-form'>
-                    <input className='input-box' placeholder='Ask Me Anything'></input>
+                    <input ref={inputQuestion} className='input-box' placeholder='Ask Me Anything'></input>
                     <div className='submit-btn-div'>
-                        <button className='submut-btn'>SUBMIT</button>
+                        <button className='submut-btn' onClick={onSubmit}>SUBMIT</button>
                     </div>
                     <div className='history-btn-div'>
                         <button className='history-btn'>HISTORY</button>
